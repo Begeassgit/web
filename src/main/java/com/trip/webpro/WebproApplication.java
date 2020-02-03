a34d7eba5f22620aaf0bf39343a596b74afcdddc
Begeassgit <begeass@sina.com>
2020-02-03 23:28:23 +0800

    first commit

---
+++ src/main/java/com/trip/webpro/WebproApplication.java
@@ -0,0 +1,13 @@
+package com.trip.webpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trip/webpro/WebproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.webpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

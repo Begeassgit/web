98663ee60cfe50bf7aabb9e509ebd411750f36fd
Begeassgit <begeass@sina.com>
2020-02-05 11:20:26 +0800

    first commit

--- src/main/java/com/trip/webpro/Controller/RegisterController.java
+++ src/main/java/com/trip/webpro/Controller/RegisterController.java
@@ -2,20 +2,32 @@ package com.trip.webpro.Controller;
 
 import com.trip.webpro.Entity.User;
 import com.trip.webpro.Service.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class RegisterController {
     public final UserService userService;
     public RegisterController(UserService userService){
         this.userService=userService;
     }
 
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public boolean register(User user){
-        return userService.register(user);
+    @RequestMapping(value = "/register")
+    public String registerIndex(){
+        return "register.html";
+    }
+
+    @RequestMapping(value = "/registerAction",method ={RequestMethod.POST,RequestMethod.GET})
+    public String register(User user){
+        if(userService.register(user))
+        {
+            return "login.html";
+        }
+        else{
+            return "error.html";
+        }
     }
 
 
